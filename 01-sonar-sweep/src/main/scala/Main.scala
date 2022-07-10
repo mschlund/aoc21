7a08bab7e62c79bc15a3e2c94a4b0fe51440bcf3
Maximilian Schlund <3297755+mschlund@users.noreply.github.com>
2022-07-10 16:38:36 +0200

    moved resources for day 01

--- 01-sonar-sweep/src/main/scala/Main.scala
+++ 01-sonar-sweep/src/main/scala/Main.scala
@@ -1,7 +1,7 @@
 import better.files._
 
 @main def hello: Unit = 
-  val input = File("src/resources/input")
+  val input = File("resources/input")
   val dirs = determineIncDec(input.contentAsString)
   val dirsWin3 = determineIncDecSliding(input.contentAsString, 3)
   println(s"Number of Incs: ${getNumberIncs(dirs)}")
